951bf63a7c505bbb33fb2d8954db36b836c9ec79
Rodrigo Luz <rodrigodsluz@gmail.com>
2024-05-07 21:14:20 -0300

    chore: update dependencies

---
+++ src/components/Map/styles.ts
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+export const MapContainer = styled.div`
+  ${() => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
+`;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,18 @@
 import { Button } from '@components/Button';
+import { Map } from '@components/Map';
 
 import * as S from './styles';
 
 export function HomeScreen() {
+  const handleOpenModal = () => {
+    alert('Hello human!');
+  };
+
   return (
     <S.Container>
-      Hello human!
-      <Button />
+      <Map />
+
+      <Button onClick={handleOpenModal} />
     </S.Container>
   );
 }
