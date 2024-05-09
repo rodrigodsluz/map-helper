ebd1c6de3a05d4ff4c999acbc6f13720120d4d0c
Rodrigo Luz <rodrigodsluz@gmail.com>
2024-05-08 21:07:36 -0300

    fix: head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
 import { HomeScreen } from '@screens/Home';
 
 export default function Home() {
-  return <HomeScreen />;
+  return (
+    <>
+      <Head>
+        <title>Map Helper</title>
+      </Head>
+
+      <HomeScreen />
+    </>
+  );
 }
